00f4093c0460bee261dddfd68178bbd878144d23
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2019-06-28 10:00:39 +0200

    Update runsync.sh

--- runsync.sh
+++ runsync.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 echo "Updating settings.ini"
 python settingsupdater.py
+
+while true
+do
+  echo "Executing sync at time : $now"
+  (cd /plexanisync && python PlexAniSync.py)
+  sleep 10
+done
+
 echo "Executing sync"
 (cd /plexanisync && python PlexAniSync.py)
