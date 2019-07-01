b6fc9b32ba18bd1d8297c6df0acb38a5575b3b0d
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2019-07-01 19:26:55 +0200

    Update runsync.sh

--- runsync.sh
+++ runsync.sh
@@ -2,6 +2,11 @@
 echo "Updating settings.ini"
 python settingsupdater.py
 
+if [ ! -z "${CUSTOM_MAPPING_URL}" ]
+then
+  wget -P /plexanisync -O custom_mappings.ini ${CUSTOM_MAPPING_URL}
+fi
+
 while true
 do
   now=$(date +"%T")
